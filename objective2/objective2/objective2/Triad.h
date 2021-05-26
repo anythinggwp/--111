e43bdfdfcacec25880f40143f5ad9b0eeb36e92a
Alexandr Shunco <OrangeKotE@yandex.ru>
2021-05-26 21:13:56 +0300

    Задание 3. Наследование
    1. Создан класс Triad (тройка чисел)
    2. Создан производный класс Triangle

---
+++ objective2/objective2/objective2/Triad.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include <cmath>
+using namespace std;
+class Triad
+{
+private:
+	double num_1;
+	double num_2;
+	double num_3;
+public:
+	
+	Triad() = default;
+	Triad(const double num1, const double num2, const double num3)
+	{
+		set_num1(num1);
+		set_num2(num2);
+		set_num3(num3);
+	}
+	Triad(const Triad& other)
+	{
+		this->num_1 = other.num_1;
+		this->num_2 = other.num_2;
+		this->num_3 = other.num_3;
+	}
+	~Triad() = default;
+	void set_num1(const double num)
+	{
+		this->num_1 = num;
+	}
+	void set_num2(const double num)
+	{
+		this->num_2 = num;
+	}
+	void set_num3(const double num)
+	{
+		this->num_3 = num;
+	}
+	double get_num1()
+	{
+		return num_1;
+	}
+	double get_num2()
+	{
+		return num_2;
+	}
+	double get_num3()
+	{
+		return num_3;
+	}
+	double sum()
+	{
+		return num_1 + num_2 + num_3;
+	}
+};
---
+++ objective2/objective2/objective2/Triangle.cpp
@@ -0,0 +1,41 @@
+﻿#include "Triangle.h"
+# define M_PI           3.14159265358979323846  /* pi */
+using namespace std;
+Triangle::Triangle(const double a, const double b, const double c)
+{
+	if (!proportion(a,b,c)) throw ("Треугольник не существует");
+		Triangle::set_num1(a);
+		Triangle::set_num2(b);
+		Triangle::set_num3(c);
+}
+double Triangle::angle_a()
+{
+	int temp = 180;
+	double rad = (pow(get_num1(),2) + pow(get_num3(),2) - pow(get_num2(),2)) / (2 * get_num1() * get_num3());
+	if (rad == 0)
+	{
+		return 90;
+	}
+	else
+	{
+		return (rad * 180) / M_PI;
+	}
+}
+double Triangle::angle_b()
+{
+	int temp = 180;
+	double rad = ((pow(get_num1(),2) + pow(get_num2(),2)) - pow(get_num3(),2)) / (2 * get_num1() * get_num2());
+	if (rad == 0)
+	{
+		return 90;
+	}
+	else
+	{
+		return (rad * temp) / M_PI;
+	}
+}
+double Triangle::angle_y()
+{
+	int temp = 180;
+	return temp - (angle_a() + angle_b());
+}
